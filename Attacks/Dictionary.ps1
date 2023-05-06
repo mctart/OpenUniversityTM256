bf927a3f05627245499e7111aef13bdfbc8aefcc
McT <1389047+mctart@users.noreply.github.com>
2023-05-06 15:26:43 +0100

    Update Dictionary.ps1

--- Attacks/Dictionary.ps1
+++ Attacks/Dictionary.ps1
@@ -1,20 +1,47 @@
 cls
-$password ="mouse"
+
+function DictionaryAttack($password, $dictionary)
+{
+    Write-Host "Attack Started"
+    foreach ($item in $dictionary)
+    {
+        if ($item -eq $password)
+        {
+            Write-Host $item " correct" -ForegroundColor Green
+            Write-Host "Password found" -ForegroundColor Green
+            Break;
+        }
+        else
+        {
+            Write-Host $item " incorrect" -ForegroundColor Red
+        }
+    }
+    Write-Host "Attack Ended"
+    Write-Host ""
+    Write-Host ""
+}
+
+#Define dictionary list
 [array]$dictionary = "dog","cat","mouse","fish"
 
-Write-Host "Dictionary attack of" $dictionary.Count "passwords"
 
-foreach ($item in $dictionary)
-{
-    if ($item -eq $password)
-    {
-        Write-Host $item " correct" -ForegroundColor Green
-        Write-Host "Password found.  Exiting program"
-        Break;
-    }
-    else
-    {
-        Write-Host $item " incorrect" -ForegroundColor Red
-    }
 
-}
+$password ="mouse"
+Write-Host "Dictionary Attack #1"
+Write-Host "using a dictionary length of" $dictionary.Count "passwords"
+Write-Host "and using known password of " 
+Write-Host $password -ForegroundColor Yellow -BackgroundColor black
+Write-Host ""
+DictionaryAttack -password $password -dictionary $dictionary
+
+
+
+$password ="otter"
+Write-Host "Dictionary Attack #2"
+Write-Host "using a dictionary length of" $dictionary.Count "passwords"
+Write-Host "and using known password of " 
+Write-Host $password -ForegroundColor Yellow -BackgroundColor black
+Write-Host ""
+
+
+DictionaryAttack -password $password -dictionary $dictionary
